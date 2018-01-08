b1325f295ce425c908c487d6a7d0b9fa4e53891c
vamshi-pulluri <33464572+vamshi-pulluri@users.noreply.github.com>
2018-01-08 18:08:30 +0100

    Add files via upload

---
+++ src/com/Exception/DataLayerException.java
@@ -0,0 +1,11 @@
+package com.Exception;
+
+public class DataLayerException extends RuntimeException {
+
+	public DataLayerException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+}
